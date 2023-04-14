c1bf365bd7094555fe2ad9affee67d0fb3f4387e
Utku Sert <93093609+utkusert@users.noreply.github.com>
2023-04-14 16:27:00 +0300

    reactive-forms-update

---
+++ my-reactive-forms/src/app/forms/form/form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  myForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email])
+  })
+  ngOnInit(): void {
+
+  }
+  onSubmit() {
+    console.log(this.myForm.value)
+  }
+
+}
